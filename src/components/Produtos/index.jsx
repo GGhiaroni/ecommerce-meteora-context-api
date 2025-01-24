1228f2683c72729acb368b540a59a44dae7017a7
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-24 19:07:49 -0300

    comecando a remover as props de pages e componentes

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -1,9 +1,9 @@
+import Titulo from "@/components/Titulo";
+import produtos from "@/mocks/produtos.json";
 import React from "react";
 import Produto from "./Produto";
-import produtos from "@/mocks/produtos.json";
-import Titulo from "@/components/Titulo";
 
-const Produtos = ({ adicionarProduto }) => {
+const Produtos = () => {
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
